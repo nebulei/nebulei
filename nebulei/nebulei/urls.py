831396926a9791c9f33ca3e78a58f0a8bd248fc8
Adrian Blanc <adrian@adrianblanc.live>
2022-07-27 23:05:32 -0500

    🎉 simple base django changes ⚡️

--- nebulei/nebulei/urls.py
+++ nebulei/nebulei/urls.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [
+
+django_urls = [
     path('admin/', admin.site.urls),
 ]
+
+third_party_urls = [
+    path('api/', include('rest_framework.urls'))
+]
+
+nebulei_urls = [
+
+]
+
+
+urlpatterns = django_urls + nebulei_urls + third_party_urls
