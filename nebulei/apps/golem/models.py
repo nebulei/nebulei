4e0028b6c0ba7e12075d18ed1698ca3528a7e4eb
Adrian Blanc <adrian@adrianblanc.live>
2022-11-07 16:06:18 -0800

    rename 📈

--- nebulei/apps/golem/models.py
+++ nebulei/apps/golem/models.py
@@ -1,60 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager ## A new class is imported. ##
-
-
-class UserManager(BaseUserManager):
-    """Define a model manager for User model with no username field."""
-
-    use_in_migrations = True
-
-    def _create_user(self, email, password, **extra_fields):
-        """Create and save a User with the given email and password."""
-        print(password)
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        print(user)
-        print(password)
-        return user
-
-    def create_user(self, email, password=None, **extra_fields):
-        print(password)
-        """Create and save a regular User with the given email and password."""
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        # print(user)
-        print(password)
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        """Create and save a SuperUser with the given email and password."""
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(email, password, **extra_fields)
+from django.contrib.auth.models import AbstractUser
 
 
 
 class User(AbstractUser):
     username = models.CharField(max_length=255)
-    email = models.EmailField(unique=True,)
-    password = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField()
     verified = models.BooleanField(default=False)
-    photo = models.URLField(blank=True)
-    header_photo = models.URLField(blank=True)
-    website = models.URLField(blank=True)
-    bio = models.CharField(max_length=160, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    date_joined = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.email
+    
