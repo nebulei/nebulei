a9b1fa485baaf1fb7d4c09d1fd6cc550c1cd057f
Adrian Blanc <adrian@adrianblanc.live>
2022-11-07 19:36:51 -0800

    basic before I commit real changes

--- nebulei/apps/golem/models.py
+++ nebulei/apps/golem/models.py
@@ -1,12 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-
-class User(AbstractUser):
-    username = models.CharField(max_length=255)
-    email = models.EmailField()
-    verified = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(auto_now=True)
-
-    
